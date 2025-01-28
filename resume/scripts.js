ba396f8fe0ba6bfe433547998361282b0c02e6b1
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2025-01-28 17:13:22 -0300

    translation

--- resume/scripts.js
+++ resume/scripts.js
@@ -9,14 +9,16 @@ const languageContent = {
         downloadCV: "Download PDF",
         viewOnlineCV: "View Online",
         flagSrc: "https://flagcdn.com/w40/us.png",
-        buttonText: "Ver em português"
+        buttonText: "Ver em português",
+        scheduleAConversation: "Let's schedule a conversation!"
     },
     pt: {
         name: "Acesse o meu currículo",
         downloadCV: "Baixar PDF",
         viewOnlineCV: "Ver Online",
         flagSrc: "https://flagcdn.com/w40/br.png",
-        buttonText: "View in English"
+        buttonText: "View in English",
+        scheduleAConversation: "Agende uma conversa comigo!"
     }
 };
 
@@ -29,6 +31,7 @@ function setLanguage(lang) {
     document.getElementById('download-cv').children[1].textContent = languageContent[lang].downloadCV;
     document.getElementById('view-online-cv').children[1].textContent = languageContent[lang].viewOnlineCV;
     document.getElementById('language-button').textContent = languageContent[lang].buttonText;
+    document.getElementById('scheduleAConversation').textContent = languageContent[lang].scheduleAConversation;
 
     // Adjust links based on the language
     document.getElementById('download-cv').href = googleDocsLinks[lang] + "/export?format=pdf";
